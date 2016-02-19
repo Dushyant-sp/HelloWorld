fe5c3da76b8970921bceff2122439f1ee409f064
Dushyant singh <dushyant.sp@airpush.com>
2016-02-19 16:31:44 +0530

    Added test class

---
+++ app/src/main/java/example/com/helloworld/Test.java
@@ -0,0 +1,22 @@
+package example.com.helloworld;
+
+import android.util.Log;
+
+
+
+/**
+ * Created by dushyant on 19/2/16.
+ */
+public class Test {
+
+    public Test(){
+        log();
+    }
+
+    void log(){
+        Log.d("TAG","Hello");
+    }
+
+
+
+}
